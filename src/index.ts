2414eb4717c92eb4b8e35a9e817a004641b96c07
Jad Khoury <jad.khoury14@gmail.com>
2022-10-30 19:23:33 +0100

    Added new exports to the 'index.ts' file

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,52 @@
 import { CustomAttributes, CustomMesages, InitialRules } from './types';
 import Validator from './validator';
 import Lang from './lang';
+import ErrorBag from './validators/errorBag';
 import RuleContract from './rules/ruleContract';
 import ImplicitRuleContract from './rules/implicitRuleContract';
-import { default as PasswordRule } from './rules/password';
+import { default as PasswordRule } from './rules/password'; 
 
 
-export class Rule extends RuleContract {};
-export class ImplicitRule extends ImplicitRuleContract {};
-export class Password extends PasswordRule {};
+class Rule extends RuleContract {};
+class ImplicitRule extends ImplicitRuleContract {};
+class Password extends PasswordRule {};
+
+function make(data: object = {}, rules: InitialRules = {}, customMessages: CustomMesages = {}, customAttributes: CustomAttributes = {}): Validator {
+    return new Validator(data, rules, customMessages, customAttributes);
+};
+
+function setDefaultLang(lang: string): void {
+    Lang.setDefaultLang(lang);
+};
+
+function setFallbackLang(lang: string): void {
+    Lang.setFallbackLang(lang);
+};
+
+function setTranslationPath(path: string): void {
+    Lang.setPath(path);
+};
+
+function setTranslationObject(translations: object): void {
+    Lang.setTranslationObject(translations);
+};
+
+export {
+    Rule,
+    ImplicitRule,
+    InitialRules,
+    Password,
+    Validator,
+    ErrorBag,
+    make,
+    setDefaultLang,
+    setFallbackLang,
+    setTranslationPath,
+    setTranslationObject,
+};
 
 export * from './rules/registerRule';
 export * from './rule';
 
-export function make(data: object = {}, rules: InitialRules = {}, customMessages: CustomMesages = {}, customAttributes: CustomAttributes = {}): Validator {
-    return new Validator(data, rules, customMessages, customAttributes);
-};
 
-export function setDefaultLang(lang: string): void {
-    Lang.setDefaultLang(lang);
-};
-
-export function setFallbackLang(lang: string): void {
-    Lang.setFallbackLang(lang);
-};
-
-export function setTranslationPath(path: string): void {
-    Lang.setPath(path);
-};
-
-export function setTranslationObject(translations: object): void {
-    Lang.setTranslationObject(translations);
-};
 
